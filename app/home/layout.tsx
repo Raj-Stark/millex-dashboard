30e94422e9107fa34a972da5739f71242dd45314
Raj <rpal778866@gmail.com>
2025-05-03 13:13:24 +0530

    chore: login fix

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,16 +1,17 @@
-"use client";
-
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar";
+import ProtectedRoute from "../components/protected-route";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger className="fixed" />
-        {children}
-      </main>
-    </SidebarProvider>
+    <ProtectedRoute>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger className="fixed" />
+          {children}
+        </main>
+      </SidebarProvider>
+    </ProtectedRoute>
   );
 }
